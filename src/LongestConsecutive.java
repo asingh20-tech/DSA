9553717e2ecc60bcade0ae07e8105b423f8483f7
Aditya Raj Singh <asingh20@oswego.edu>
2026-09-11 20:34:06 -0400

    added solution 2 also 90ms

--- src/LongestConsecutive.java
+++ src/LongestConsecutive.java
@@ -1,6 +1,6 @@
 import java.util.Arrays;
 
-
+// Solution 1
 public int LongestConsecutive(int[] nums) {
     if (nums.length == 0) {
         return 0;
@@ -28,5 +28,43 @@ public int LongestConsecutive(int[] nums) {
     return Math.max(max, count);
 }
 
+// Solution 2
 
+
+//class Solution {
+//    public int longestConsecutive(int[] nums) {
+//
+//        if (nums.length == 0) {
+//            return 0;
+//        }
+//
+//        Arrays.sort(nums);
+//
+//        int count = 1;
+//        int max = 1;
+//
+//        for (int i = 0; i < nums.length - 1; i++) {
+//
+//            if (nums[i + 1] - nums[i] == 1) {
+//                count++;
+//            }
+//            else if (nums[i + 1] == nums[i]) {
+//                continue;
+//            }
+//            else {
+//                if (count > max) {
+//                    max = count;
+//                }
+//
+//                count = 1;
+//            }
+//        }
+//
+//        if (count > max) {
+//            max = count;
+//        }
+//
+//        return max;
+//    }
+//}
 
